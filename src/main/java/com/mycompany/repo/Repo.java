ee0c08749341cbe82b26f94c900b1e0c6f115578
JoseLuis2510 <113859269+JoseLuis2510@users.noreply.github.com>
2023-09-09 09:46:10 -0600

    Cambio2

--- src/main/java/com/mycompany/repo/Repo.java
+++ src/main/java/com/mycompany/repo/Repo.java
@@ -13,5 +13,6 @@ public class Repo {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hola");
+        System.out.println("Hello");
     }
 }
